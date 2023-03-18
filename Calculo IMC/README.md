09697c80e43d2bb6d42e36293a5ec74352294ef1
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-03-17 21:09:08 -0300

    Calculo IMC

---
+++ Calculo IMC/README.md
@@ -0,0 +1,5 @@
+# Cálculo IMC
+
+<a href="https://www.figma.com/file/0maYji0hkbhRICqGEpWu9M/IMC-(Copy)?node-id=6%3A4&t=wvlbyOe7ICTWuGtz-1">
+    <img src="./assets/screen.png">
+</a>
---
+++ Calculo IMC/js/main.js
@@ -0,0 +1,40 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+import { calculateIMC, notNumber } from './utils.js'
+
+const form = document.querySelector("form")
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+const modalWrapper = document.querySelector('.modal-wrapper')
+const modalMessage = document.querySelector('.modal .tittle span')
+const modalBtnClose = document.querySelector('.modal button.close')
+
+inputHeight.oninput = () => AlertError.close()
+inputWeight.oninput = () => AlertError.close()
+
+form.onsubmit = e => {
+    e.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+
+    if (weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}       
+
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+    
+    Modal.message.innerText = message
+    Modal.open()
+}
