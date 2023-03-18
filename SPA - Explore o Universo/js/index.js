34b50099edbd9d5b0b50b6d7e726495d77b0da3b
Fernanda Palaria <fernanda_palaria@terceiros.sicredi.com.br>
2023-03-18 05:20:28 -0300

    SPA finalizado

--- SPA - Explore o Universo/js/index.js
+++ SPA - Explore o Universo/js/index.js
@@ -1,15 +1,40 @@
-// console.log("construir página")
-// setTimeout(() => console.log("ler html"), 1000)
-// console.log("mostrar")
-
 import { Router } from "./router.js"
 
 const router = new Router()
 router.add('/', "/pages/home.html")
-router.add('/about', "/pages/about.html")
+router.add('/universo', "/pages/universo.html")
+router.add('/exploracao', "/pages/exploracao.html")
 router.add(404, "/pages/404.html")
 
 router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+const homeMenu = document.querySelector('#home')
+const universoMenu = document.querySelector('#universo')
+const exploracaoMenu = document.querySelector('#exploracao')
+const body = document.querySelector('body')
+
+homeMenu.addEventListener('click', () => {
+    toggleMenuBold(homeMenu)
+    body.style.backgroundImage = 'url(./assets/bg-home.png)'
+})
+
+universoMenu.addEventListener('click', () => {
+    toggleMenuBold(universoMenu)
+    body.style.backgroundImage = 'url(./assets/bg-universo.png)'
+})
+
+exploracaoMenu.addEventListener('click', () => {
+    toggleMenuBold(exploracaoMenu)
+    body.style.backgroundImage = 'url(./assets/bg-exploração.png)'
+})
+
+function toggleMenuBold(clickedMenu) {
+    homeMenu.classList.remove('menuSelected')
+    universo.classList.remove('menuSelected')
+    exploracao.classList.remove('menuSelected')
+
+    clickedMenu.classList.add('menuSelected')
+}
